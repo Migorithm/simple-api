085cff8953196f815d50a7f1dd052663d3a7fae3
Migo <saka90030@gmail.com>
2024-09-06 16:54:22 +0400

    remove rider related stuff

--- api/obj/Debug/net8.0/api.AssemblyInfo.cs
+++ api/obj/Debug/net8.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+52cc613b1b711253ce9c11cc4e289e27bf71238b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fdbf3c27a3634796cab7eeec04cefaf8e7271ac3")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
